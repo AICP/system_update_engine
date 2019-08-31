85ff6d5a3e32dfcb9328d2ac5dfb8217d83e2154
Surge Raval <Surge1223@gmail.com>
2019-08-31 11:36:34 +0200

    Ignore timestamp check in DeltaPerformer
    Change-Id: I744addf6a87025515040cdad203c235c4a678de9

--- payload_consumer/delta_performer.cc
+++ payload_consumer/delta_performer.cc
@@ -1774,6 +1774,7 @@ ErrorCode DeltaPerformer::ValidateManifest() {
     }
   }
 
+  /*
   if (manifest_.max_timestamp() < hardware_->GetBuildTimestamp()) {
     LOG(ERROR) << "The current OS build timestamp ("
                << hardware_->GetBuildTimestamp()
@@ -1785,6 +1786,7 @@ ErrorCode DeltaPerformer::ValidateManifest() {
     LOG(INFO) << "The current OS build allows downgrade, continuing to apply"
                  " the payload with an older timestamp.";
   }
+ */
 
   if (major_payload_version_ == kChromeOSMajorPayloadVersion) {
     if (manifest_.has_dynamic_partition_metadata()) {
